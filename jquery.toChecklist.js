fdae5b1764f73719abae2ea0529e8151df5d7da1
Scott Horlbeck <me@scotthorlbeck.com>
2008-10-15 18:37:40 +0000

    Updated the comments to include details on license.

--- jquery.toChecklist.js
+++ jquery.toChecklist.js
@@ -1,13 +1,22 @@
 /**
- * toChecklist - a jQuery Plugin
- * Version 1.0 Beta
- *
- * This is a work in progress and will be released under the GPL when completed.
- *
+ * toChecklist plugin for jQuery 1.2.6+
  * @author Scott Horlbeck <me@scotthorlbeck.com>
+ * @url http://www.scotthorlbeck.com/code/tochecklist
+ * @version 1.0 Beta
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Full license is available at <http://www.gnu.org/copyleft/gpl.html>.
  *
 */
-
 jQuery.fn.toChecklist = function(o) { // "o" stands for options
 
 	// Provide default settings, which may be overridden if necessary.
